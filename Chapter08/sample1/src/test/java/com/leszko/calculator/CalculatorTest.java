8fd52fca8961eedd9ba61e19c84c65e4cd612285
Minh Le <minhtri54cntt@gmail.com>
2026-07-09 23:34:58 +0000

    feature branch test

--- Chapter08/sample1/src/test/java/com/leszko/calculator/CalculatorTest.java
+++ Chapter08/sample1/src/test/java/com/leszko/calculator/CalculatorTest.java
@@ -4,11 +4,11 @@ import static org.junit.Assert.assertEquals;
 
 public class CalculatorTest {
      private Calculator calculator = new Calculator();
-     private CalculatorApplication calculatorApplication = new CalculatorApplication();   
-     private CalculatorController calculatorController = new CalculatorController();
+     //private CalculatorApplication calculatorApplication = new CalculatorApplication();   
+     //private CalculatorController calculatorController = new CalculatorController();
 
      @Test
      public void testSum() {
-          assertEquals(5, calculator.sum(2, 3));
+          assertEquals(2, calculator.sum(2, 3));
      }
 }
